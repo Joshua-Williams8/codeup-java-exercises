15e28d97ec59ecbccf0c2b38e00ce12e4c309fb0
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-05-22 20:24:14 -0500

    very basic

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    String josh = "Josh";
+    Number num1 = 3;
+
+    //josh = 123;
+    System.out.print("Hello, World!");
+  }
+}
+//
