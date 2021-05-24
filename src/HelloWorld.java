82563018369c64b7ddbba3ab997068f0d550e794
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-05-23 23:14:00 -0500

    some reading

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
+
 public class HelloWorld {
   public static void main(String[] args) {
     String josh = "Josh";
     Number num1 = 3;
+    //Array fre = [141,41]
 
     //josh = 123;
     System.out.print("Hello, World!");
