1100fa77acf174038fb1af84c58c88185b191260
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-07-08 13:39:19 -0500

    adding notes

--- src/zLectAnotations/Fruit.java
+++ src/zLectAnotations/Fruit.java
@@ -1,13 +1,13 @@
 package zLectAnotations;
 
-public class Fruit implements Food{
+public class Fruit implements Food {
   //  Since this implements food, it has to have a taste() method defined.
 
 
   private String flavor;
 
   @Deprecated
-  public void tasteBitter(){
+  public void tasteBitter() {
     System.out.println("ew");
   }
 //The deprecated lets java know we are getting rid of this method and need to stop using it.
@@ -21,15 +21,15 @@ public class Fruit implements Food{
   //@Override is meant to override a method in a super class. This isn't required to do it, but it shows other devs that you want to use this instead.
   @Override
 //  Below is needed because we are impleting Food
-  public String taste(){
+  public String taste() {
     return this.flavor;
   }
 
+  //We can test our method below.
   public static void main(String[] args) {
     Fruit lime = new Fruit();
     lime.tasteBitter(); //See the difference? It's showing that it's been marked for "removal"
     lime.flavor = "oof";
-
 
 
   }
