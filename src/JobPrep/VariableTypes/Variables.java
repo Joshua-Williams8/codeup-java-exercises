8f70c7e41b8734ae841bcb9a864e7b570be5dcfa
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-09-05 11:40:21 -0500

    review Java for work

--- src/JobPrep/VariableTypes/Variables.java
+++ src/JobPrep/VariableTypes/Variables.java
@@ -3,5 +3,14 @@ package JobPrep.VariableTypes;
 //There are 3 variable types!
 //Local, Instance, and Static!/Class varaible.
 public class Variables {
+//  The variable is declared using a data type with the identifier name ex:
+  String aVar = "variable";
+//  It can be initialized at the time of declaration or it can be assigned a value taken from the user.
+
+//  TODO: Java Instance Variable
+//  Variables that are declared inside the class but OUTSIDE the scope of the any method area called instance variables in Java.
+//  Can be initialized at the time of the class loading or when an object of the class is created.
+//  Instance variables of different types have default values that are specified in the next point.
+  public String aInstanceVar = "Instance!";
 
 }
