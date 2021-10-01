d3c3715840302f20717cb205b75fdd2ce0f112e5
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-10-01 08:48:28 -0500

    review for work

--- src/Electronics/Dell.java
+++ src/Electronics/Dell.java
@@ -1,5 +1,13 @@
 package Electronics;
 
 public class Dell extends Laptop {
+  public Dell(){
+    setMake("Dell");
+  }
+  public Dell(String model){
+    setMake("Dell");
+    setModel(model);
+  }
+
 
 }
